cf6fc09df02767d95af396ecf943d4d98af5d541
ScriptingOtaku <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2022-04-16 12:51:08 -0700

    camera works but is like bad

--- src/client/Modules/CameraHandler.lua
+++ src/client/Modules/CameraHandler.lua
@@ -1,4 +1,3 @@
-local RunService = game:GetService("RunService")
 --[[
     @ScriptingOtaku
 
@@ -10,6 +9,7 @@ local RunService = game:GetService("RunService")
 local camera_handler = {}
 
 local Camera = workspace.CurrentCamera
+local Changed = nil
 
 function set_scriptable() --> void
     Camera.CameraType = Enum.CameraType.Scriptable
@@ -18,22 +18,60 @@ function set_scriptable() --> void
     until Camera.CameraType == Enum.CameraType.Scriptable
 end
 
-function calculate(size: Vector2, offset: Vector3, rotation: Vector3) --> CFrame
-    -- Make the camera fit the entire game board
-    --TODO: 
+function getVerticalFov(camera)
+    -- by ThanksRoBama: https://devforum.roblox.com/t/forcing-a-certain-aspect-ratio-for-the-camera-on-all-clients/390845/2
+	return camera.FieldOfView
+	
+	--Equivalent calculation:
+	--local z = camera.NearPlaneZ
+	--local viewSize = camera.ViewportSize
+	--
+	--local r0, r1 = 
+	--	camera:ViewportPointToRay(viewSize.X/2, viewSize.Y*0, z), 
+	--	camera:ViewportPointToRay(viewSize.X/2, viewSize.Y*1, z)
+	--	
+	--return math.deg(math.acos(r0.Direction.Unit:Dot(r1.Direction.Unit)))
 end
 
-function camera_handler:start(size: Vector2, offset: Vector3, rotation: Vector3) --> void
+function getHorizontalFov(camera)
+    -- by ThanksRoBama: https://devforum.roblox.com/t/forcing-a-certain-aspect-ratio-for-the-camera-on-all-clients/390845/2
+    local z = camera.NearPlaneZ
+    local viewSize = camera.ViewportSize
+    
+    local r0, r1 = 
+        camera:ViewportPointToRay(viewSize.X*0, viewSize.Y/2, z), 
+        camera:ViewportPointToRay(viewSize.X*1, viewSize.Y/2, z)
+        
+    return math.deg(math.acos(r0.Direction.Unit:Dot(r1.Direction.Unit)))
+end
+
+function setHorizontalFov(camera, fov)
+    -- by ThanksRoBama: https://devforum.roblox.com/t/forcing-a-certain-aspect-ratio-for-the-camera-on-all-clients/390845/2
+    local aspectRatio = getHorizontalFov(camera)/getVerticalFov(camera)
+    
+    camera.FieldOfView = fov / aspectRatio
+end
+
+function camera_handler:start(size: Vector2, tile_size: number, rotation: Vector3) --> void
 
     set_scriptable()
+    setHorizontalFov(Camera, 80)
 
-    RunService:BindToRenderStep("Camera", Enum.RenderPriority.Camera.Value, function()
-        Camera.CFrame = calculate(size, offset, rotation)
+    local position = Vector3.new(
+        (size.X * tile_size / 2 - 2),
+        30,
+        (size.Y * tile_size / 2 - 2) - (rotation.X / 3)
+    )
+    Camera.CFrame = CFrame.new(position) * CFrame.Angles(math.rad(rotation.X), math.rad(rotation.Y), math.rad(rotation.Z))
+    
+    Changed = Camera:GetPropertyChangedSignal("ViewportSize"):Connect(function()
+        Camera.CFrame = CFrame.new(position) * CFrame.Angles(math.rad(rotation.X), math.rad(rotation.Y), math.rad(rotation.Z))
+        setHorizontalFov(Camera, 80)
     end)
 end
 
 function camera_handler:stop()
-    RunService:UnbindFromRenderStep("Camera")
+    Changed:Disconnect()
 end
 
 return camera_handler
--- src/client/client.client.lua
+++ src/client/client.client.lua
@@ -4,4 +4,12 @@ local PlacementHandler = require(Modules.PlacementHandler)
 local CameraHandler = require(Modules.CameraHandler)
 
 PlacementHandler:start()
-CameraHandler:center()
+
+--[[
+    12x8 = 22,50,14
+]]
+CameraHandler:start(
+    Vector2.new(12, 8),
+    4,
+    Vector3.new(-60, 0, 0)
+)
