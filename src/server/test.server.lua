744c042412f26b6b19ea9d1e8d80953219a3a67c
ScriptingOtaku <56370697+DillonSumnerEpicGamer@users.noreply.github.com>
2022-04-16 16:44:49 -0700

    got rid of thing

--- src/server/test.server.lua
+++ src/server/test.server.lua
@@ -1,32 +1,6 @@
-local Players = game:GetService("Players")
-
 local Modules = script.Parent.Modules
 
 local GameManager = require(Modules.GameManager)
-
-function separate_string(string: string, separator: string) --> Table
-   local result: table = {}
-   
-   if separator == nil then
-      separator = "%s"
-   end
-   for match in string.gmatch(string, "([^" .. separator .. "]+)") do
-      table.insert(result, match)
-   end
-    return result
-end
-
-Players.PlayerAdded:Connect(function(player: Player)
-    player.Chatted:Connect(function(message: string)
-        local sep: table = separate_string(message, " ")
-        local command: string = sep[1]
-        local _args: table = table.remove(sep, 1)
-
-        if command == "!start" then
-            GameManager:start_game(player)
-        end
-    end)
-end)
 
 task.wait(1)
 
